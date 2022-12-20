146f67f25a2e008b1e96920bef8f54ca43689f86
Seba Hohlgit config --global user.email sebahohl06@gmail.com <sebahohl06@gmail.com>
2022-12-20 15:14:19 -0300

    Segunda entrega

--- src/App.js
+++ src/App.js
@@ -1,18 +1,30 @@
+import React, { useState, useContext} from 'react';
+import {Sidebar, Header, CartItem} from './components'
+import { useFetch } from './hooks/useFetch';
+import { Link } from 'react-router-dom';
+import { URL_BASE, URL_ENDPOINTS } from './constants/services';
+import Router from './router';
 import './App.css';
-import ItemListContainer from './components/ItemListContainer/ItemListContainer';
-import Navbar from './components/Navbar/Navbar';
+import { CartProvider } from './context';
 
-function App() {
+const App = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const [showProducts, setShowProducts] = useState(false);
+  const { data: user, error, loading } = useFetch(`${URL_BASE}${URL_ENDPOINTS.USERS}`);
+
+  const onHandlerCart = () => {
+    setIsOpen(!isOpen);
+  }
+
   return (
-    <div className="App">
-      <header>
-        <Navbar/>
-      </header>
-  <body>
-    <ItemListContainer/>
-  </body>
+    <div className="container">
+      <CartProvider>
+      <Sidebar onClose={onHandlerCart} isOpen={isOpen} />
+      <Header onHandlerCart={onHandlerCart} user={user[0]} />
+      <Router />
+      </CartProvider>
     </div>
-  );
+  )
 }
 
 export default App;
